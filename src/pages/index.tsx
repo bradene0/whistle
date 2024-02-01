054454469401b36ecd6290c9c50ca90af10c238a
Braden E <bradene0@outlook.com>
2024-02-01 17:30:35 -0600

    Fixed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Link from "next/link";
+//import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
