ea1328ce1d6934a2b92f460e35db76e64dc28212
Braden E <bradene0@outlook.com>
2024-01-31 18:13:31 -0600

    Added auth through clerk w/ sign in/out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -18,6 +20,7 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+          <div>{!User && <SignInButton/>}{!!User && <SignOutButton/>}</div>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
