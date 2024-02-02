103a3ba9b9418a2ccc64ee08eb2e2e269c6acf95
Braden E <bradene0@outlook.com>
2024-02-01 20:22:19 -0600

    styling and once again broken posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,32 @@
 //import { z } from "zod";
-
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import { userAgent } from "next/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id, name: user.username, profilePicture: user.profileImageUrl
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+          take: 100,
+  });
+
+  const users = (
+    await clerkClient.users.getUserList({
+    userId: posts.map((post) => post.authorID),
+    limit: 100 
+  })
+  ).map(filterUserForClient);
+
+  return posts.map(post => ({
+    post,
+    author: users.find((user)) => user.id === post.authorId),
+   }));
   }),
 });
 
